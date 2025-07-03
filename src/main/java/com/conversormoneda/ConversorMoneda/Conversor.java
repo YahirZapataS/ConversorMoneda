d8ff5143b4bb8bf6246298e042fa29f9edc236d8
YahirZapataS <yahirzapatae@gmail.com>
2025-07-03 00:34:15 -0600

    Update http's configurations

--- src/main/java/com/conversormoneda/ConversorMoneda/Conversor.java
+++ src/main/java/com/conversormoneda/ConversorMoneda/Conversor.java
@@ -2,6 +2,7 @@ package com.conversormoneda.ConversorMoneda;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Conversor {
 
@@ -10,7 +11,7 @@ public class Conversor {
     public static void userMenu() throws IOException, InterruptedException {
         while (true) {
             System.out.println("========== Conversor de Moneda ==========");
-            System.out.println("1. Convertir Moneda \n2. Salir");
+            System.out.println("1. Convertir Moneda \n2.Salir");
             System.out.println("Seleccione una opción: ");
             String opcion = scanner.nextLine();
 
@@ -28,11 +29,22 @@ public class Conversor {
     }
 
     private static void realizarConversion() throws IOException, InterruptedException {
+
+        Set<String> codigosValidos = ConversorApp.obtenerCodigosValidos();
+
         System.out.println("Ingrese la moneda a convertir (Ejemplo: USD, MXN): ");
         String moneda = scanner.nextLine().toUpperCase();
+        if(!codigosValidos.contains(moneda)) {
+            System.out.println("Código de moneda inválido.\n Consulta los códigos de moneda en 2.");
+            return;
+        }
 
         System.out.println("Ingrese a que moneda le gustaría convertir (Ejemplo: USD, MXN): ");
         String conversion = scanner.nextLine().toUpperCase();
+        if(!codigosValidos.contains(conversion)) {
+            System.out.println("Código de moneda inválido.\\nConsulta los códigos de moneda en 2.");
+            return;
+        }
 
         System.out.println("Ingrese el monto a convertir: ");
         double monto;
--- src/main/java/com/conversormoneda/ConversorMoneda/ConversorApp.java
+++ src/main/java/com/conversormoneda/ConversorMoneda/ConversorApp.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Set;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -13,10 +14,9 @@ public class ConversorApp {
 
     private static final String API_KEY = "e4267433fe40fbb90f42e079";
 
-    public static double obtenerTasa(String base, String destino) throws IOException, InterruptedException {
-        String url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/" + base + "/" + destino;
+    public static double obtenerTasa(String moneda, String conversion) throws IOException, InterruptedException {
+        String url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/" + moneda + "/" + conversion;
 
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .timeout(java.time.Duration.ofSeconds(10))
@@ -24,11 +24,18 @@ public class ConversorApp {
                 .GET()
                 .build();
 
+        HttpClient client = HttpClient.newHttpClient();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+        int statusCode = response.statusCode();
+        System.out.println("Código HTTP: " + statusCode);
+
         if (response.statusCode() != 200) {
-            throw new RuntimeException("Error en la solicitud HTTP. Código: " + response.statusCode());
+            throw new RuntimeException("Error: código de estado HTTP: " + statusCode);
         }
+
+        String cuerpo = response.body();
+        System.out.println("Respuesta JSON: " + cuerpo);
 
         Gson gson = new Gson();
         JsonObject json = gson.fromJson(response.body(), JsonObject.class);
@@ -38,5 +45,26 @@ public class ConversorApp {
         }
 
         return json.get("conversion_rate").getAsDouble();
+    }
+
+    public static Set<String> obtenerCodigosValidos() throws IOException, InterruptedException {
+        String url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/USD";
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .timeout(java.time.Duration.ofSeconds(10))
+            .header("Accept", "application/json")
+            .GET()
+            .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(response.statusCode() != 200)
+                throw new RuntimeException("HTTP Error al cargar monedas: " + response.statusCode());
+
+            JsonObject json = new Gson().fromJson(response.body(), JsonObject.class);
+            JsonObject conversionRates = json.getAsJsonObject("conversion_rates");
+
+            return conversionRates.keySet();
     }
 }
